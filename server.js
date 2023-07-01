def0e07da267164cabd1327189c0a59e85215060
wensaqt <116151112+wensaqt@users.noreply.github.com>
2023-07-01 10:16:59 +0200

    mangodb connection w/ express.js is done, time to build the back office

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/whiteboardsdb', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://127.0.0.1:27017/whiteboardsdb', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 
--- src/db.js
+++ src/db.js
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const url = 'mongodb://localhost:27017/whiteboardsdb';
+// const url = 'mongodb://localhost:27017/whiteboardsdb';
 
-export async function connectToDatabase() {
-  try {
-    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('Database Connected');
-  } catch (err) {
-    console.log('Database Connection Error:', err);
-  }
-};
+// export async function connectToDatabase() {
+//   try {
+//     await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+//     console.log('Database Connected');
+//   } catch (err) {
+//     console.log('Database Connection Error:', err);
+//   }
+// };
