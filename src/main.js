96318fd5af98bf3a3b39f9b0c2da60bd9b88c39a
wensaqt <116151112+wensaqt@users.noreply.github.com>
2023-07-01 08:07:12 +0200

    added mongodb back office connection

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import connectToDatabase from './db.js';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faList, faTrash, faStickyNote, faPlus, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
@@ -13,6 +14,17 @@ library.add(faStickyNote)
 library.add(faPlus)
 library.add(faChevronLeft)
 library.add(faChevronRight)
+
+connectToDatabase()
+  .then(() => {
+    console.log('Connected to MongoDB');
+    createApp(App)
+      .component('font-awesome-icon', FontAwesomeIcon)
+      .mount('#app');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
 
